018e046058ba0a9554014bed9828e8d94427f559
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-08-22 09:11:17 +0100

    Add files via upload

---
+++ res/scripts/index.js
@@ -0,0 +1,12 @@
+
+
+const navTogglera = document.querySelector(".nav-toggler");
+
+navTogglera.addEventListener('click', navToggle);
+
+function navToggle(){
+
+    const nav = document.querySelector(".navbar");
+    nav.classList.toggle("navbarbaa");
+   
+}
